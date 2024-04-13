c05da55040e59d5c97a39ba0818dd26817e393fb
rshakash <akashr.ug23.me@nitp.ac.in>
2024-04-13 16:28:52 +0530

    Add database configuration

---
+++ dbConfig.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+const connectionString = process.env.DB_URL;
+
+const pool = new Pool({
+    connectionString: connectionString
+});
+
+module.exports = { pool };
